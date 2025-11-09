87a3fab91634b128362c77a83ff5581ef9147340
ecstranaut <gotham47g@gmail.com>
2025-11-09 14:27:41 +0530

    Added Image to ClipboardItem

--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,121 @@
 // Standard Crates
 #[allow(unused)]
 use std::{
-    collections::{HashMap, VecDeque}, error::Error, fmt, thread::sleep, time::Duration
+    fmt,
+    collections::{
+        HashMap, 
+        VecDeque
+    }, 
+    error::Error, 
+    thread::{
+        self,
+        sleep
+    }, 
+    time::Duration, 
+    io::{
+        stdin, 
+        stdout, 
+        Write
+    },
+    sync::{
+        Arc, 
+        Mutex,
+        atomic::{
+            AtomicBool, 
+            Ordering
+        }
+    }
 };
 
 // External Crates
-use arboard::Clipboard;
+use arboard::{Clipboard};
+use termion::{
+    event::Key, 
+    input::TermRead, 
+    raw::IntoRawMode
+};
 
+// For image viewing
+use viuer::{Config};
+use image::DynamicImage;
 
 // // ---------------------------- Error --------------------------------
-// #[derive(Debug)]
-// #[allow(unused)]
-// // Error when you try to overwrite a Pos
-// enum PosError {
-//     PosOverlap {
-//         pos: u8,
-//         existing_content: String,
-//         new_content: String
-//     },
-//     PosJump {
-//         prev_pos: u8,
-//         pos: u8
-//     }
-// }
+#[derive(Debug)]
+#[allow(unused)]
+// Error when you try to overwrite a Pos
+enum ClipboardErr {
+    ClipboardEmpty
+}
 
-// // Displays for the Errors
-// impl fmt::Display for PosError {
-//     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-//         match self {
-//             PosError::PosOverlap {pos, existing_content, new_content} => {
-//                 write!(f, "POS ALREADY EXISTS! POS: {} | EXISTING CONTENT: {} | NEW CONTENT: {}", pos, existing_content, new_content)
-//             },
-//             PosError::PosJump {prev_pos, pos} => {
-//                 write!(f, "WARNING! POS JUMP FOUND! PREVIOUS POS: {} | CURRENT POS: {}", prev_pos, pos)
-//             }
-//         }
-//     }
-// }
+// Displays for the Errors
+impl fmt::Display for ClipboardErr {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            ClipboardErr::ClipboardEmpty => {
+                write!(f, "Clipboard is empty. Please add copy something before trying again.")
+            }
+        }
+    }
+}
 
-// // Implement the structs as Errors
-// impl Error for PosError {}
+// Implement the structs as Errors
+impl Error for ClipboardErr {}
 // // -------------------------------------------------------------------
 
 
-// ---------------------- Clipboard Structs --------------------------
+// ------------------------- Clipboard Item -----------------------------
 // Note: Using "C" for now.
 // Consider using specific types from arboard later on.
 #[allow(unused)]
 #[derive(Debug, Clone, PartialEq)] // PartialEQ needed for comparision
 enum ClipboardItem {
-    Text(String)
-}
-
-impl fmt::Display for ClipboardItem {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            ClipboardItem::Text(s) => write!(f, "{}", s),
-        }
+    Text(String),
+    Image {
+        width: usize,
+        height: usize,
+        bytes: Vec<u8>
     }
 }
 
-#[allow(unused)]
-struct ClipboardHistory {
-    history: VecDeque<ClipboardItem>,
-    max_size: usize,
+// Make the item printable
+impl fmt::Display for ClipboardItem {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            ClipboardItem::Text(s) => write!(f, "{}", s.replace('\n', "\r\n")),
+            ClipboardItem::Image {width, height, bytes} => {
+
+                let img = image::RgbaImage::from_raw(
+                    *width as u32,
+                    *height as u32,
+                    bytes.to_owned()
+                ).unwrap();
+
+                let dynamic_img = DynamicImage::ImageRgba8(img);
+
+                // Print to terminal
+                let conf = Config {
+                    width: Some(80),
+                    height: Some(24),
+                    ..Default::default()
+                };
+
+                viuer::print(&dynamic_img, &conf).unwrap();
+
+                write!(f, "")
+            }
+        }
+    }
 }
 // -------------------------------------------------------------------
 
 
 // --------------------- Hist Implementation -------------------------
+#[allow(unused)]
+struct ClipboardHistory {
+    history: VecDeque<ClipboardItem>,
+    max_size: usize,
+}
+
 #[allow(unused)]
 impl ClipboardHistory {
     fn new(max_size: usize) -> Self {
@@ -120,7 +167,14 @@ impl fmt::Display for ClipboardHistory {
         
         // No sorting needed! Just iterate.
         for (pos, item) in self.history.iter().enumerate() {
-            printable += &format!("\r\n{}       | {}     ", pos, item);
+            match item {
+                ClipboardItem::Image { width, height, .. } => {
+                    printable += &format!("\r\n{}       | Image ({}, {})     ", pos, width, height);
+                },
+                ClipboardItem::Text(string) => {
+                    printable += &format!("\r\n{}       | {}     ", pos, string.to_string());
+                }
+            }
         }
         
         write!(f, "{printable}")
@@ -129,34 +183,100 @@ impl fmt::Display for ClipboardHistory {
 // -------------------------------------------------------------------
 
 
+// ------------------- Clipboard Implementations ---------------------
+#[allow(unused)]
+trait GetItem {
+    fn get_item(&mut self) -> Result<ClipboardItem, ClipboardErr>;
+}
+
+impl GetItem for Clipboard {
+    fn get_item(&mut self) -> Result<ClipboardItem, ClipboardErr> {
+        if let Ok(img_dat) = self.get_image() {
+            Ok(ClipboardItem::Image { 
+                width: img_dat.width, 
+                height: img_dat.height, 
+                bytes: img_dat.bytes.to_vec()
+            })
+        } else if let Ok(str_data) = self.get_text() {
+            Ok(ClipboardItem::Text(str_data))
+        } else {
+            Err(ClipboardErr::ClipboardEmpty)
+        }
+    }
+}
+// -------------------------------------------------------------------
+
+
 // -------------------- Monitor, just for fun ------------------------
 #[allow(unused)]
 trait Monitor {
-    fn monitor(&mut self);
+    fn monitor(self);
 }
 
 impl Monitor for Clipboard {
     // Usage:
-    // let mut clipboard = Clipboard::new().unwrap();
+    // let clipboard = Clipboard::new().unwrap();
     // clipboard.monitor();
     
-    fn monitor(&mut self) {
-        println!("Starting to monitor. Press 'CTRL + C' in terminal to quit.");
+    fn monitor(self) {        
+        let stop = Arc::new(AtomicBool::new(false));
 
-        let mut previous_content = self.get_text().unwrap();
-        loop {
-            let content = self.get_text().unwrap();
-            
-            if content != previous_content {
-                println!("<--------------------->");
-                println!("Clipboard Change Detected: {}", self.get_text().unwrap());
-                previous_content = content;
+        let kb_stop = stop.clone();
+        let cm_stop = stop.clone();
+        
+
+        let kb_handle = thread::spawn(move || {
+            let stdin = stdin();
+            let mut stdout = stdout().into_raw_mode().unwrap();
+
+            write!(stdout, "{}{}", termion::clear::All, termion::cursor::Goto(1, 1)).unwrap();
+            stdout.flush().unwrap();
+
+            write!(stdout, "Monitoring Clipboard. Press 'q' to exit. \r\n").unwrap();
+            stdout.flush().unwrap();
+
+
+            for c in stdin.keys() {
+                if kb_stop.load(Ordering::SeqCst) {
+                    break;
+                }
+
+                if let Ok(Key::Char('q')) | Ok(Key::Char('Q')) = c {
+                    kb_stop.store(true, Ordering::SeqCst);
+                    break;
+                }
             }
+        });
+        
+        let clipboard = Arc::new(Mutex::new(self));
+        
+        let cm_handle = thread::spawn(move || {
+            let mut stdout = stdout().into_raw_mode().unwrap();
 
-            // To prevent overloading? 
-            // (perhaps not an issue in rust like it is on python)
-            sleep(Duration::from_millis(100));
-        }
+            let mut previous_content = clipboard.lock().unwrap().get_item().unwrap();
+            
+            while !cm_stop.load(Ordering::SeqCst) {
+                sleep(Duration::from_millis(100));
+
+                if let Ok(content) = clipboard.lock().unwrap().get_item() {
+                    if content != previous_content {
+                        write!(stdout, "{}{}", termion::clear::All, termion::cursor::Goto(1, 1)).unwrap();
+                        
+                        write!(stdout, "Monitoring Clipboard. Press 'q' to exit. \r\n").unwrap();
+                        stdout.flush().unwrap();
+
+                        write!(stdout, "\n\nClipboard Change Detected:\r\n\n```\r\n{}\r\n```\r\n", content).unwrap();
+                        stdout.flush().unwrap();
+                        
+                        previous_content = content;
+                    }
+                }
+            }
+        });
+
+        kb_handle.join().unwrap();
+        cm_handle.join().unwrap();
+
     }
 }
 // -------------------------------------------------------------------
@@ -164,11 +284,14 @@ impl Monitor for Clipboard {
 
 // ----------------------------- Main --------------------------------
 fn main() {
-    let mut ch = ClipboardHistory::new(20);
-    ch.add(ClipboardItem::Text("miaow".to_string()));
-    ch.add(ClipboardItem::Text("woof".to_string()));
-    ch.add(ClipboardItem::Text("rawr".to_string()));
-    ch.promote(1);
-    println!("{ch}");
+    // let mut ch = ClipboardHistory::new(20);
+    // ch.add(ClipboardItem::Text("miaow".to_string()));
+    // ch.add(ClipboardItem::Text("woof".to_string()));
+    // ch.add(ClipboardItem::Text("rawr".to_string()));
+    // ch.promote(1);
+    // println!("{ch}");
+
+    let clipboard = Clipboard::new().unwrap();
+    clipboard.monitor(); // <- Consumes Clipboard. Do not use for polling
 }
 // -------------------------------------------------------------------
